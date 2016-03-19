cc92319eb72faacaf62ac41a28c6b5e43ba09308
Nancy <nancy.pcrz@gmail.com>
2016-03-18 18:21:20 -0600

    se agregó carpeta de FOD Detalle

---
+++ app/states/agent/desarrollo/FOD/detalle/fodDetalle.controller.js
@@ -0,0 +1,8 @@
+angular.module( 'moose' ).controller( 'fodDetalleController', fodDetalleController );
+
+fodDetalleController.$inject = [];
+
+function fodDetalleController(){
+  var vm = this;
+
+}
